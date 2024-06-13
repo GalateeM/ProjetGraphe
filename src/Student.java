e5492643a7a0be6a47f2972b572c044959d7ba38
Amazir CHAOUCHI <amazir.chaouchi@gmail.com>
2024-06-13 11:43:20 +0200

    Add implementation of executeCeremony() and executeReject()

--- src/Student.java
+++ src/Student.java
@@ -1,9 +1,27 @@
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Student extends Participant<School> {
 	
 	public Student(int id) {
 		super(id);
 	}
+	
+	public School getBestPreference() {		
+		School preferedSchool = null;
+		
+		Iterator<Map.Entry<School, Integer>> iterator = this.preferences.entrySet().iterator();
+		while(iterator.hasNext() && preferedSchool == null) {
+			Map.Entry<School, Integer> currentPreference = iterator.next();
+			if(currentPreference.getValue() == this.rejections.size() + 1) {
+				preferedSchool = currentPreference.getKey();
+			}
+		}
+		
+		return preferedSchool;
+	}
+	
 	
 	public String toString() {
 		return "" + getId();
--- src/StudentMatingStrategy.java
+++ src/StudentMatingStrategy.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 
 public class StudentMatingStrategy implements MatingStrategy {
 
@@ -5,18 +8,57 @@ public class StudentMatingStrategy implements MatingStrategy {
 	
 	public StudentMatingStrategy(MatchingManager<School, Student> manager) {
 		this.matchingManager = manager;
+		
+		
 	}
 	
 	@Override
 	public void executeCeremony() {
-		// TODO Auto-generated method stub
-		
+		for(Student student : this.matchingManager.getE2List()) {		
+			if(this.matchingManager.getRejected().contains(student)) {
+ 				 School preferedSchool = (School)student.getBestPreference(); 				 
+ 				 if(this.matchingManager.getCurrentAssociation().get(preferedSchool) == null) {
+					 this.matchingManager.getCurrentAssociation().put(preferedSchool, new HashSet<Student>());
+				 }
+ 				 if(preferedSchool != null) {
+ 					 this.matchingManager.getCurrentAssociation().get(preferedSchool).add(student); 					 
+ 				 }
+			}
+		 }
 	}
 
 	@Override
 	public void executeReject() {
-		// TODO Auto-generated method stub
-		
+		//clear all rejected
+		this.matchingManager.getRejected().removeAll(this.matchingManager.getRejected());
+
+		//Loop on the students
+		for (Map.Entry<School, HashSet<Student>> association : this.matchingManager.getCurrentAssociation().entrySet()) {
+			HashSet<Student> studentSet = association.getValue();
+			School school = association.getKey();
+
+			if(studentSet.size() > school.getCapacity()) {
+				//create a list prefered students
+				// !!!!! Must return a LIST !!!!!
+				List<Student> preferedStudents = school.getPreferenceAmongList(studentSet);
+
+				//update the rejections
+				for(Student student : studentSet) {
+					if(!preferedStudents.contains(student)) {
+						student.addReject(school);
+						this.matchingManager.getRejected().add(student);
+					}
+				}
+
+				//replace the list with only the prefered ones
+				for(Student student : preferedStudents) {
+					HashSet<Student> oneElementSet = new HashSet<Student>();
+					oneElementSet.add(student);
+					this.matchingManager.getCurrentAssociation().put(school, oneElementSet);
+				}
+			}
+
+		}
 	}
 	
 	@Override
